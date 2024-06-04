c3628ed18d617ebcf6d617f3dd065ce66b67f1a2
devhoainamtd <truongdinhhoainam1703@gmail.com>
2024-06-04 10:07:32 +0700

    update home.html

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,26 +1,92 @@
-// Hiển thị sub-nav ở header
-$(document).ready(function(){
-    $('.nav-item.dropdown').hover(function(){
-        $(this).find('.sub-menu').stop(true, true).slideDown(300); /* Hiển thị sub-menu tương ứng với thẻ li */
-    }, function(){
-        $(this).find('.sub-menu').stop(true, true).slideUp(100); /* Ẩn sub-menu khi hover ra khỏi thẻ li */
-    });
+/*--------------------------------------------------------------
+# Header
+--------------------------------------------------------------*/
+/**
+ * Sticky header on scroll
+ */
+const selectHeader = document.querySelector("#header");
+if (selectHeader) {
+  document.addEventListener("scroll", () => {
+    window.scrollY > 100
+      ? selectHeader.classList.add("sticked")
+      : selectHeader.classList.remove("sticked");
+  });
+}
+
+/**
+ * Mobile nav toggle
+ */
+
+const mobileNavToogleButton = document.querySelector(".mobile-nav-toggle");
+
+if (mobileNavToogleButton) {
+  mobileNavToogleButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    mobileNavToogle();
+  });
+}
+
+function mobileNavToogle() {
+  document.querySelector("body").classList.toggle("mobile-nav-active");
+  mobileNavToogleButton.classList.toggle("bi-list");
+  mobileNavToogleButton.classList.toggle("bi-x");
+}
+
+/**
+ * Hide mobile nav on same-page/hash links
+ */
+document.querySelectorAll("#navbar a").forEach((navbarlink) => {
+  if (!navbarlink.hash) return;
+
+  let section = document.querySelector(navbarlink.hash);
+  if (!section) return;
+
+  navbarlink.addEventListener("click", () => {
+    if (document.querySelector(".mobile-nav-active")) {
+      mobileNavToogle();
+    }
+  });
 });
 
-// function rotateImage() {
-//     $('.rotating-image').css({ transform: 'rotate(0deg)' }).animate(
-//         { deg: 360 },
-//         {
-//             duration: 20000,
-//             step: function(now) {
-//                 $(this).css({ transform: 'rotate(' + now + 'deg)' });
-//             },
-//             complete: rotateImage // Lặp lại animation
-//         }
-//     );
-// }
+/**
+ * Toggle mobile nav dropdowns
+ */
+const navDropdowns = document.querySelectorAll(".navbar .dropdown > a");
 
-// rotateImage(); // Khởi động animation
+navDropdowns.forEach((el) => {
+  el.addEventListener("click", function (event) {
+    if (document.querySelector(".mobile-nav-active")) {
+      event.preventDefault();
+      this.classList.toggle("active");
+      this.nextElementSibling.classList.toggle("dropdown-active");
+
+      let dropDownIndicator = this.querySelector(".dropdown-indicator");
+    //   dropDownIndicator.classList.toggle("bi-chevron-up");
+      dropDownIndicator.classList.toggle("bi-chevron-down");
+    }
+  });
+});
+/**
+ * Open and close the search form.
+ */
+const searchOpen = document.querySelector(".js-search-open");
+const searchClose = document.querySelector(".js-search-close");
+const searchWrap = document.querySelector(".js-search-form-wrap");
+
+searchOpen.addEventListener("click", (e) => {
+  e.preventDefault();
+  searchWrap.classList.add("active");
+});
+
+searchClose.addEventListener("click", (e) => {
+  e.preventDefault();
+  searchWrap.classList.remove("active");
+});
+
+/*--------------------------------------------------------------
+# End Header
+--------------------------------------------------------------*/
+
 
 function rotate() {
     $('.rotating-bg').css({ transform: 'rotate(0deg)' }).animate({ borderSpacing: 360 }, {
